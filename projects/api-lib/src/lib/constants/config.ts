d6eca18036a0941cba09baac78c6fbfafea7901f
junaid5546 <lafmk22@gmail.com>
2024-11-23 01:11:47 +0000

    api url mapped to gulmall.com

--- projects/api-lib/src/lib/constants/config.ts
+++ projects/api-lib/src/lib/constants/config.ts
@@ -1,5 +1,3 @@
-export const BASICTOKEN = 'eXV6ZWVfY2xpZW50OjI5MDIzNmNmLTgxZDItNDg5MS1hYmNlLWYzZmUzYzA5NWMxMA==';
-export const SERVER_IP = 'https://gulemall-34vbtsxcwa-zf.a.run.app';
-export const CLOUDFLARE_API = 't_vovFtqT_xfP4OantM_xTnUL7ftImvg_LCYrySt';
-export const CLOUDFLARE_ACCOUNT_ID = 'faa438f6541a4860550243762c092167';
-//export const SERVER_IP = 'backend-development.digitalmall.app';
+export const BASICTOKEN='eXV6ZWVfY2xpZW50OjI5MDIzNmNmLTgxZDItNDg5MS1hYmNlLWYzZmUzYzA5NWMxMA==';
+export const SERVER_IP='https://gulemall-34vbtsxcwa-zf.a.run.app';
+export const MEDIA_IP='https://media.gulmall.com/upload';
